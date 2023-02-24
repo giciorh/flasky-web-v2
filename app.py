de4b64a7b494ca50f952ea7f830e953223270a10
giciorh <126015418+giciorh@users.noreply.github.com>
2023-02-24 21:46:28 +0000

    added some dynamic data and simlpe api

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Wrocław',
+  'salary': '10_000 zł'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Kraków',
+  'salary': '13_000 zł'
+}, {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': 'Gdańsk',
+  'salary': '9_000 zł'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Katowice',
+}, {
+  'id': 5,
+  'title': 'Junior Python Dev',
+  'location': 'Poznań',
+  'salary': '6_000 zł'
+}]
+
+
+#dodano make api
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 @app.route("/")
 def hello_world():
-  return render_template("hello.html")
+  return render_template("hello.html", jobs=JOBS, company_name='Flasky')
 
 
 if __name__ == '__main__':
